bd33d27904477965ce725cf659a927020b6f1980
cobject <cobject@gmail.com>
2019-04-23 15:02:08 +0900

    feature: Simple home page

---
+++ precache-manifest.5d9543e51e8369e0fe7e6502b105bea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d8cefb42481cbf4563e2cff1d12571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33da74bcc2273db4d6ab",
+    "url": "/static/css/main.bafa0919.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "33da74bcc2273db4d6ab",
+    "url": "/static/js/main.fda80ef6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
